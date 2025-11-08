ab8c1559a71e64d2cba36ecf9e8332f4e0ddd437
Shashank-Vinoo <shashankvinoo.imp@gmail.com>
2025-11-08 11:13:40 +0000

    Delete VHDL RISC-V Single-Cycle CPU directory

--- VHDL RISC-V Single-Cycle CPU/README.md
+++
@@ -1,2 +0,0 @@
-This is my single cycle CPU updated last August 18th
-
--- VHDL RISC-V Single-Cycle CPU/RISC-V single cycle/Adder.v
+++
@@ -1,7 +0,0 @@
-module Adder(
-	input wire[31:0] PC,
-	input wire[31:0] Immediate,
-	output wire [31:0] Sum
-);
-	assign Sum = PC+Immediate;
-endmodule
